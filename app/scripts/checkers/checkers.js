32bdd7fdf4409e1b7e6a05510f9e940a3fe46d62
clarknoah <noahb08@gmail.com>
2014-01-12 11:38:20 -0500

    fu

---
+++ app/scripts/checkers/checkers.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function Checkers() {
+
+    function Player() {
+        this.playerName;
+        this.playerColor;
+        this.playerPieces;
+    };
+
+    function Checkerboard() {
+        this.grid = [
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}],
+            [{}, {}, {}, {}, {}, {}, {}, {}]
+        ];
+    };
+
+    function Piece() {
+        //Where the piece is on the board
+        this.pieceLocation;
+
+        //Where or not the piece is currently
+        this.pieceStatus;
+
+    };
+
+    function Game() {
+        this.
+    };
+    //Player Object
+    //Checkerboard Object
+    //piece object
+    //Game object
+
+
+
+
+    this.endGame = function() {
+
+    };
+
+
+    this.startGame = function() {
+
+    };
+
+    this.endGame = function() {
+
+    };
+
+}
